e552c1c68111a349a2a5a498a221f21a6e11d714
RudraNarayanHota-143 <89252181+RudraNarayanHota-143@users.noreply.github.com>
2022-10-06 15:40:36 +0530

    Update check.cpp

--- check.cpp
+++ check.cpp
@@ -1,19 +1,56 @@
-/*  C Program to find area and circumference of circle  */
+import java.util.Arrays;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-#include<stdio.h>
+/**
+ * Java program to remove duplicates from this array. You don't
+ * need to physically delete duplicate elements, replacing with null, or
+ * empty or default value is ok.
+ *
+ * @author http://javarevisited.blogspot.com
+ */
+public class TechnicalInterviewTest {
 
-int main()
-{
-    float area, radius, circum;
+    private static final Logger logger = LoggerFactory.getLogger(TechnicalInterviewTest.class);
 
-    printf("ENTER THE RADIUS OF THE CIRCLE :: ");
-    scanf("%f", &radius);
-    area = 3.142 * radius * radius;
-    circum = 2 * 3.142 * radius;
-    printf("\nTHE AREA OF THE CIRCLE IS :: %f", area);
-    printf("\n\nTHE CIRCUMFERENCE OF THE CIRCLE IS :: %f\n", circum);
+    public static void main(String args[]) {
 
-    return 0;
+        int[][] test = new int[][]{
+            {1, 1, 2, 2, 3, 4, 5},
+            {1, 1, 1, 1, 1, 1, 1},
+            {1, 2, 3, 4, 5, 6, 7},
+            {1, 2, 1, 1, 1, 1, 1},};
+
+        for (int[] input : test) {
+            System.out.println("Array with Duplicates       : " + Arrays.toString(input));
+            System.out.println("After removing duplicates   : " + Arrays.toString(removeDuplicates(input)));
+        }
+    }
+
+    /*
+     * Method to remove duplicates from array in Java, without using
+     * Collection classes e.g. Set or ArrayList. Algorithm for this
+     * method is simple, it first sort the array and then compare adjacent
+     * objects, leaving out duplicates, which is already in the result.
+     */
+    public static int[] removeDuplicates(int[] numbersWithDuplicates) {
+
+        // Sorting array to bring duplicates together      
+        Arrays.sort(numbersWithDuplicates);     
+      
+        int[] result = new int[numbersWithDuplicates.length];
+        int previous = numbersWithDuplicates[0];
+        result[0] = previous;
+
+        for (int i = 1; i < numbersWithDuplicates.length; i++) {
+            int ch = numbersWithDuplicates[i];
+
+            if (previous != ch) {
+                result[i] = ch;
+            }
+            previous = ch;
+        }
+        return result;
+
+    }
 }
-
-
